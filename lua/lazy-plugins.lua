30045c90c6d5cb0b0a8799048da4bf8bce27e005
Samuel Cristobal <el.tobal@gmail.com>
2024-01-29 11:30:48 +0100

    New theme

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -129,10 +129,27 @@ require('lazy').setup({
 
   {
     -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
+    -- 'navarasu/onedark.nvim',
+
+    -- Warmer alternative to Onedark
+    -- 'NLKNguyen/papercolor-theme',
+
+    -- 'folke/tokyonight.nvim',
+
+    -- 'bluz71/vim-nightfly-colors',
+
+    'rebelot/kanagawa.nvim',
+
+
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'onedark'
+      -- vim.cmd.colorscheme 'onedark'
+      -- vim.cmd.colorscheme 'PaperColor'
+      -- vim.cmd.colorscheme 'tokyonight'
+
+      -- vim.cmd.colorscheme 'nightfly'
+
+      vim.cmd.colorscheme 'kanagawa'
     end,
   },
 
