355dc52fb56d0631cd90df1ab2825be6344d1e2a
Samuel Cristobal <samuel.cristobal@protonmail.com>
2024-09-27 21:23:13 +0200

    Update nvim

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -8,20 +8,29 @@ vim.wo.number = true
 
 return {
 
-    -- transparent background
-    'tribela/vim-transparent',
+  -- transparent background
+  'tribela/vim-transparent',
 
-    -- Relative line numbers
-    'sitiom/nvim-numbertoggle',
+  -- Relative line numbers
+  'sitiom/nvim-numbertoggle',
 
-    {
-        "nvim-neo-tree/neo-tree.nvim",
-        branch = "v3.x",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-            "MunifTanjim/nui.nvim",
-            -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-        }
-    }
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = 'v3.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+      'MunifTanjim/nui.nvim',
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+  },
+  {
+    'amrbashir/nvim-docs-view',
+    lazy = true,
+    cmd = 'DocsViewToggle',
+    opts = {
+      position = 'right',
+      width = 60,
+    },
+  },
 }
--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -109,6 +109,16 @@ return {
           --  For example, in C this would take you to the header.
           map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
+          map('<leader>h', vim.lsp.buf.hover, '[S]how [H]over info')
+
+          -- vim.diagnostic.config {
+          --  virtual_text = false,
+          -- }
+
+          -- Show line diagnostics automatically in hover window
+          -- vim.o.updatetime = 250
+          -- vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
+
           -- The following two autocommands are used to highlight references of the
           -- word under your cursor when your cursor rests there for a little while.
           --    See `:help CursorHold` for information about when this is executed
