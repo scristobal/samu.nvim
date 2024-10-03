413ab5d9a2f5ed7216639d6c4d3c63c5bf32f013
Samuel Cristobal <samuel.cristobal@protonmail.com>
2024-10-03 19:22:54 +0200

    Added nvim themes

--- lua/custom/plugins/theme.lua
+++ lua/custom/plugins/theme.lua
@@ -1,27 +1,23 @@
 return {
-    {
-        -- Theme inspired by Atom
-        -- 'navarasu/onedark.nvim',
-
-        -- Warmer alternative to Onedark
-        -- 'NLKNguyen/papercolor-theme',
-
-        -- 'folke/tokyonight.nvim',
-
-        -- 'bluz71/vim-nightfly-colors',
-
-        'rebelot/kanagawa.nvim',
-
-
-        priority = 1000,
-        config = function()
-            -- vim.cmd.colorscheme 'onedark'
-            -- vim.cmd.colorscheme 'PaperColor'
-            -- vim.cmd.colorscheme 'tokyonight'
-
-            -- vim.cmd.colorscheme 'nightfly'
-
-            vim.cmd.colorscheme 'kanagawa'
-        end,
-    },
+  {
+    -- 'navarasu/onedark.nvim',
+    -- 'NLKNguyen/papercolor-theme',
+    -- 'folke/tokyonight.nvim',
+    -- 'bluz71/vim-nightfly-colors',
+    -- 'rebelot/kanagawa.nvim',
+    -- 'rose-pine/neovim',
+    -- 'AlexvZyl/nordic.nvim',
+    'bluz71/vim-moonfly-colors',
+    priority = 1000,
+    config = function()
+      -- vim.cmd.colorscheme 'onedark'
+      -- vim.cmd.colorscheme 'PaperColor'
+      -- vim.cmd.colorscheme 'tokyonight'
+      -- vim.cmd.colorscheme 'nightfly'
+      -- vim.cmd.colorscheme 'kanagawa-dragon'
+      -- vim.cmd.colorscheme 'rose-pine'
+      -- vim.cmd.colorscheme 'nordic'
+      vim.cmd.colorscheme 'moonfly'
+    end,
+  },
 }
