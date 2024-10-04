87c4fa86380ece83030c3b81812b572e91e9683e
Samuel Cristobal <samuel.cristobal@protonmail.com>
2024-10-04 21:14:23 +0200

    [nvim] Added lock file

--- lua/custom/plugins/theme.lua
+++ lua/custom/plugins/theme.lua
@@ -7,8 +7,9 @@ return {
     -- 'rebelot/kanagawa.nvim',
     -- 'rose-pine/neovim',
     -- 'AlexvZyl/nordic.nvim',
-    'bluz71/vim-moonfly-colors',
-    -- 'Tsuzat/NeoSolarized.nvim',
+    -- 'bluz71/vim-moonfly-colors',
+    'Tsuzat/NeoSolarized.nvim',
+    -- 'RRethy/base16-nvim',
     priority = 1000,
     config = function()
       -- vim.cmd.colorscheme 'onedark'
@@ -18,8 +19,9 @@ return {
       -- vim.cmd.colorscheme 'kanagawa-dragon'
       -- vim.cmd.colorscheme 'rose-pine'
       -- vim.cmd.colorscheme 'nordic'
-      vim.cmd.colorscheme 'moonfly'
-      -- vim.cmd.colorscheme 'NeoSolarized'
+      -- vim.cmd.colorscheme 'moonfly'
+      vim.cmd.colorscheme 'NeoSolarized'
+      -- vim.cmd.colorscheme 'base16-default-dark'
     end,
   },
 }
