55621197954a4ec9bb1329b6c21c7c8e917307f9
Samuel Cristobal <samuel.cristobal@protonmail.com>
2024-10-07 21:12:52 +0200

    [nvim] Config lua line

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -41,5 +41,13 @@ return {
   {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
+    opts = {
+      options = {
+        theme = 'auto',
+        component_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' },
+      },
+      sections = { lualine_b = { { 'branch', icon = '' } } },
+    },
   },
 }
