b29a17fa5c1302bca032dd81115006cd7e96239a
Samuel Cristobal <samuel.cristobal@protonmail.com>
2025-01-12 10:09:53 +0100

    Custom setup

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,66 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+
+return {
+
+  -- Relative line numbers
+  -- 'sitiom/nvim-numbertoggle',
+
+  -- show code docs on a separate pannel
+  {
+    'amrbashir/nvim-docs-view',
+    lazy = true,
+    cmd = 'DocsViewToggle',
+    opts = {
+      position = 'right',
+      width = 60,
+    },
+  },
+
+  -- custom tabs
+  -- {
+  --   'nanozuki/tabby.nvim',
+  --   config = function()
+  --     vim.o.showtabline = 2
+  --   end,
+  -- },
+
+  -- surround words/blocks/whatever with " ' <tag> and so
+  {
+    'kylechui/nvim-surround',
+    event = 'VeryLazy',
+    config = function()
+      require('nvim-surround').setup {}
+    end,
+  },
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    opts = {
+      options = {
+        theme = 'auto',
+        component_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' },
+      },
+      sections = { lualine_b = { { 'branch', icon = '' } } },
+    },
+  },
+  {
+    'petertriho/nvim-scrollbar',
+    opts = {},
+  },
+
+  -- trigger available moves on current line
+  --  {
+  --    'tris203/precognition.nvim',
+  --    opts = {},
+  --  },
+
+  -- shows missed oportunities for optimal moves
+  --  {
+  --    'm4xshen/hardtime.nvim',
+  --    dependencies = { 'MunifTanjim/nui.nvim', 'nvim-lua/plenary.nvim' },
+  --    opts = {},
+  --  },
+}
---
+++ lua/custom/plugins/theme.lua
@@ -0,0 +1,30 @@
+return {
+  -- 'navarasu/onedark.nvim',
+  -- 'NLKNguyen/papercolor-theme',
+  -- 'folke/tokyonight.nvim',
+  -- 'bluz71/vim-nightfly-colors',
+  -- 'rebelot/kanagawa.nvim',
+  -- 'rose-pine/neovim',
+  -- 'AlexvZyl/nordic.nvim',
+  -- 'bluz71/vim-moonfly-colors',
+  -- 'Tsuzat/NeoSolarized.nvim',
+  'RRethy/base16-nvim',
+  -- 'EdenEast/nightfox.nvim',
+  -- 'nyoom-engineering/oxocarbon.nvim',
+
+  priority = 1000,
+  config = function()
+    -- vim.cmd.colorscheme 'onedark'
+    -- vim.cmd.colorscheme 'PaperColor'
+    -- vim.cmd.colorscheme 'tokyonight'
+    -- vim.cmd.colorscheme 'nightfly'
+    -- vim.cmd.colorscheme 'kanagawa-dragon'
+    -- vim.cmd.colorscheme 'rose-pine'
+    -- vim.cmd.colorscheme 'nordic'
+    -- vim.cmd.colorscheme 'moonfly'
+    -- vim.cmd.colorscheme 'NeoSolarized'
+    vim.cmd.colorscheme 'base16-default-dark'
+    -- vim.cmd.colorscheme 'carbonfox'
+    -- vim.cmd.colorscheme 'oxocarbon'
+  end,
+}
