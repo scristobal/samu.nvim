cab1adcba2b0dab8fcc445bf3e4a196bddb60cb9
Samuel Cristobal <samuel.cristobal@protonmail.com>
2025-03-08 09:37:34 +0100

    Fix colortheme race condition, removed unused plugins

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -76,17 +76,17 @@ return {
   --    opts = {},
   --  },
 
-  {
-    'https://github.com/fresh2dev/zellij.vim',
-    -- Pin version to avoid breaking changes.
-    -- tag = '0.3.*',
-    lazy = false,
-    init = function()
-      -- Options:
-      -- vim.g.zelli_navigator_move_focus_or_tab = 1
-      -- vim.g.zellij_navigator_no_default_mappings = 1
-    end,
-  },
+  -- {
+  --   'https://github.com/fresh2dev/zellij.vim',
+  --   -- Pin version to avoid breaking changes.
+  --   -- tag = '0.3.*',
+  --   lazy = false,
+  --   init = function()
+  --     -- Options:
+  --     -- vim.g.zelli_navigator_move_focus_or_tab = 1
+  --     -- vim.g.zellij_navigator_no_default_mappings = 1
+  --   end,
+  -- },
 
   --  {
   --    'rachartier/tiny-inline-diagnostic.nvim',
@@ -123,5 +123,5 @@ return {
   },
 
   -- Trouble pannel
-  'folke/trouble.nvim',
+  -- 'folke/trouble.nvim',
 }
