221d05344a38726b17d6a26beaaacd13d745db06
Samuel Cristobal <samuel.cristobal@protonmail.com>
2025-03-09 09:17:08 +0100

    Python LSPs

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -232,7 +232,8 @@ return {
       local servers = {
         clangd = {},
         -- gopls = {},
-        -- pyright = {},
+        pyright = {},
+        ruff_lsp = {},
         rust_analyzer = {
           checkOnSave = {
             allFeatures = true,
@@ -307,10 +308,12 @@ return {
         handlers = {
           function(server_name)
             local server = servers[server_name] or {}
+
             -- This handles overriding only values explicitly passed
             -- by the server configuration above. Useful when disabling
             -- certain features of an LSP (for example, turning off formatting for ts_ls)
             server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+
             require('lspconfig')[server_name].setup(server)
           end,
         },
