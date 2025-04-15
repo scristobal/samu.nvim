6f80d8af08d1a47e5fb41f73b6d200c017793d8b
Samuel Cristobal <samuel.cristobal@protonmail.com>
2025-04-15 22:44:18 +0200

    lsp restore

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -304,17 +304,17 @@ return {
       -- You can add other tools here that you want Mason to install
       -- for you, so that they are available from within Neovim.
       --
-      --  local ensure_installed = vim.tbl_keys(servers or {})
-      --  vim.list_extend(ensure_installed, {
-      --    'stylua', -- Used to format Lua code
-      --    'markdownlint', --
-      --    'clang-format',
-      --  })
-      --  require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+      local ensure_installed = vim.tbl_keys(servers or {})
+      vim.list_extend(ensure_installed, {
+        'stylua', -- Used to format Lua code
+        'markdownlint', --
+        'clang-format',
+      })
+      require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
       require('mason-lspconfig').setup {
         ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
-        automatic_installation = true,
+        automatic_installation = false,
         handlers = {
           function(server_name)
             local server = servers[server_name] or {}
